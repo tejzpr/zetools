0320a4322598c97392c077901e127aead47b87a4
tejzpr <tejzpr@gmail.com>
2022-04-19 13:21:02 -0700

    Update

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 
 	"github.com/tejzpr/zetools/commands"
 
+	// Available command plugins
 	_ "github.com/tejzpr/zetools-base64"
 	_ "github.com/tejzpr/zetools-checkport"
 	_ "github.com/tejzpr/zetools-hmac"
 	_ "github.com/tejzpr/zetools-ls"
 	_ "github.com/tejzpr/zetools-ping"
 	_ "github.com/tejzpr/zetools-tail"
+
+	// End of available command plugins
 
 	cli "github.com/tejzpr/zcli/v2"
 )
